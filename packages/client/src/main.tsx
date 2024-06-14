16465d1d37936e6faf675187f4414c7a2d335aa6
Hotaru <biyuehuya@gmail.com>
2024-06-14 15:07:15 +0800

    feat: hash router

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'antd/dist/reset.css';
 import 'tailwindcss/tailwind.css';
 import 'antd/dist/antd.min.js.LICENSE.txt';
@@ -14,8 +14,8 @@ notification.config({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
